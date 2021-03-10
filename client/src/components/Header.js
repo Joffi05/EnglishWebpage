69eb594a1d4aaa0695fcc1e5aa7fd4d31ba9f151
GenossenCoder <xperte9988@gmail.com>
2021-03-11 00:07:56 +0100

    added Header

---
+++ client/src/components/Header.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import logo from './Logo.jpg'
+import { FaSearch } from 'react-icons/fa'
+const Header = () => {
+    return (
+        <header style={HeaderStyle}>
+            <img src={logo} alt="Logo" style={LogoStyle}/>
+            <i className="input-group-prepend"><FaSearch/></i>
+            <input style={InputStyle}placeholder="Search..."></input>
+        </header>
+    )
+}
+const HeaderStyle = {
+    backgroundColor: 'white',
+    boxShadow: '0px 8px 15px rgba(0, 0, 0, 0.5)',
+    padding:'1px',
+    margin:'0',
+}
+const LogoStyle = {
+    width: '60px',
+}
+const InputStyle ={
+    padding: '15px',
+    width: '30%',
+    margin:'12px',
+    marginLeft: '30%',
+    position: 'absolute',
+    fontSize: '18px',
+}
+export default Header
