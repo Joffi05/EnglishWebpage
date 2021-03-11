d2d3981cdeaa20d0346cd7bc82755b42d6a5bbf9
GenossenCoder <xperte9988@gmail.com>
2021-03-11 19:35:50 +0100

    modified:   src/components/Header.js 	new file:   src/components/Sidebar.js 	modified:   src/styles/Header.module.css 	new file:   src/styles/Sidebar.module.css

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component} from 'react'
 import Header from './components/Header.js'
+import Sidebar from './components/Sidebar.js'
 
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Sidebar/>
     </div>
   );
 }
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import logo from '.././pngs/logo.png'
-import { FaSearch } from 'react-icons/fa'
-import styles from '.././styles/Header.css'
+import styles from '.././styles/Header.module.css'
 
 const Header = () => {
     return (
         <header className={styles.header}>
             <img className={styles.logo} src={logo} alt="Logo"/>
-            <i className="input-group-prepend" ><FaSearch/></i>
             <input className={styles.input} placeholder="Search..."></input>
         </header>
     )
