bf479692321c41873b474b841190b3d692e229ae
Jonathan <joffi0566@gmail.com>
2021-03-11 21:51:49 +0100

    "little change"

--- client/src/components/firebase.js
+++ client/src/components/firebase.js
@@ -1,14 +1,13 @@
 import firebase from "firebase"
 
 firebase.initializeApp({
-    apiKey: "AIzaSyB4nFLOSH2WOe63Bam8M8sufsHrctMnoqw",
-    authDomain: "englishwebsite-e7339.firebaseapp.com",
-    databaseURL: "https://englishwebsite-e7339-default-rtdb.firebaseio.com",
-    projectId: "englishwebsite-e7339",
-    storageBucket: "englishwebsite-e7339.appspot.com",
-    messagingSenderId: "319585569362",
-    appId: "1:319585569362:web:2bbb036cc588e04fc42d8b",
-    measurementId: "G-5W4BT66G3Z"
+    apiKey: "AIzaSyB_Oeas64Xw_fV0gYI5C49HiNjXng4llp0",
+    authDomain: "englishapp-6c6ce.firebaseapp.com",
+    projectId: "englishapp-6c6ce",
+    storageBucket: "englishapp-6c6ce.appspot.com",
+    messagingSenderId: "424254810282",
+    appId: "1:424254810282:web:a8e7c106c049cc454e08d3",
+    measurementId: "G-5VTDXFW7W3"
 })
 
 export default firebase
