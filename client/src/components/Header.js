36b4c939c5232e7603691dae9fa3beed60e2da87
Jonathan <joffi0566@gmail.com>
2021-03-11 15:34:34 +0100

    modified:   client/src/components/Header.js 	modified:   client/src/styles/Header.js.css

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,31 +1,16 @@
 import React from 'react'
 // import logo from './Logo.jpg'
-// import { FaSearch } from 'react-icons/fa'
+import { FaSearch } from 'react-icons/fa'
+import styles from '.././styles/Header.js.css'
 
 const Header = () => {
     return (
-        <header style={HeaderStyle}>
-            {/* <img src={logo} alt="Logo" style={LogoStyle}/> */}
-            {/* <i className="input-group-prepend"><FaSearch/></i> */}
-            <input style={InputStyle}placeholder="Search..."></input>
+        <header className={styles.header}>
+            {/* <img src={logo} alt="Logo" style={styles.logoStyle}/> */}
+            <i className="input-group-prepend" ><FaSearch/></i>
+            <input style={styles.inputStyle} placeholder="Search..."></input>
         </header>
     )
 }
-const HeaderStyle = {
-    backgroundColor: 'white',
-    boxShadow: '0px 8px 15px rgba(0, 0, 0, 0.5)',
-    padding:'1px',
-    margin:'0',
-}
-const LogoStyle = {
-    width: '60px',
-}
-const InputStyle ={
-    padding: '15px',
-    width: '30%',
-    margin:'12px',
-    marginLeft: '30%',
-    position: 'absolute',
-    fontSize: '18px',
-}
+
 export default Header
