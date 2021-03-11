777827f92da815a0fbf450c4fa8cc97f60cafbbe
Jonathan <joffi0566@gmail.com>
2021-03-11 16:17:38 +0100

    added logo

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,12 +1,12 @@
 import React from 'react'
-// import logo from './Logo.jpg'
+import logo from '.././pngs/logo.png'
 import { FaSearch } from 'react-icons/fa'
-import styles from '.././styles/Header.js.css'
+import styles from '.././styles/Header.css'
 
 const Header = () => {
     return (
         <header className={styles.header}>
-            {/* <img src={logo} alt="Logo" style={styles.logoStyle}/> */}
+            <img src={logo} alt="Logo" style={styles.logoStyle}/>
             <i className="input-group-prepend" ><FaSearch/></i>
             <input className={styles.input} placeholder="Search..."></input>
         </header>
