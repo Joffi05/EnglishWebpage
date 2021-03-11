343250652cba69808afef3dc6e63ae2364caec11
Jonathan <joffi0566@gmail.com>
2021-03-11 20:44:44 +0100

    lennart fett

--- server/index.js
+++ server/index.js
@@ -1,13 +1,62 @@
-// server/index.js
-const firebase = require('firebase')
+import * as functions from 'firebase-functions'
+import * as admin from 'firebase-admin'
+import * as express from 'express'
 
-const express = require("express");
-const PORT = process.env.PORT || 3001;
-const app = express();
+admin.initializeApp(functions.config().firebase)
+const db = admin.firestore();
+const postCollection = 'posts';
 
+const express = require("express")
+const PORT = process.env.PORT || 3001
+const app = express()
 
 
 app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+  console.log(`Server listening on ${PORT}`)
 });
 
+
+
+//define google cloud function name
+export const webApi = functions.https.onRequest(main);
+
+
+
+//POST endpoints
+
+
+// Create new Post
+app.post('/newPost', async (req, res) => {
+  try {
+    const post = {
+      name: req.body['name'],
+      date: req.body['date'],
+      city: req.body['city'],
+      text: req.body['text']
+    }
+
+    const newDoc = await db.collection(postCollection).add(post)
+    res.status(201).send('Created new post!')
+  }
+  catch (err) {
+    res.status(400).send(`Fill all fields!`)
+    console.log(err)
+  }
+})
+
+
+
+//GET endpoints
+
+
+// Get all posts
+app.get('/posts', async (req, res) => {
+  try {
+    const allPosts = await db.collection(postCollection).get()
+
+    res.status(200).json(allPosts)
+  }
+  catch (err) {
+    res.status(500).send(err)
+  }
+})
