3493e1cde4f3be4c650b977db6516e66a0e9af57
Jonathan <joffi0566@gmail.com>
2021-03-11 15:54:45 +0100

    modified:   client/src/components/Header.js 	renamed:    client/src/styles/Header.js.css -> client/src/styles/Header.css 	modified:   package-lock.json 	modified:   package.json

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,14 +1,14 @@
 import React from 'react'
 // import logo from './Logo.jpg'
 import { FaSearch } from 'react-icons/fa'
-import styles from '.././styles/Header.js.css'
+import styles from '.././styles/Header.css'
 
 const Header = () => {
     return (
         <header className={styles.header}>
             {/* <img src={logo} alt="Logo" style={styles.logoStyle}/> */}
             <i className="input-group-prepend" ><FaSearch/></i>
-            <input style={styles.inputStyle} placeholder="Search..."></input>
+            <input className={styles.input} placeholder="Search..."></input>
         </header>
     )
 }
