26ad12ec9b8ae888ed22b9f72d000fbad7d80cca
GenossenCoder <xperte9988@gmail.com>
2021-03-12 21:43:52 +0100

    Fixed a Form-height issue, added like Button and a Placeholder for the News Feature

---
+++ client/src/components/News.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import styles from '.././styles/News.module.css'
+
+const News = () => {
+    return (
+        <form>
+            <h1 className={styles.Header}></h1>
+            <div className={styles.News1}>
+                Feature is coming soon...
+            </div>
+            <div className={styles.News2}>
+                
+            </div>
+            <div className={styles.News3}>
+                
+            </div>
+        </form>
+    )
+}
+
+export default News
--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import styles from '.././styles/Sidebar.module.css'
 import MakePost from './MakePost.js'
+import News from './News.js'
 
 const Sidebar = () => {
     return (
         <div className={styles.Sidebar}>
             <form className={styles.Form}>
                 <MakePost/>
+            </form>
+            <form>
+                <News/>
             </form>
         </div>
     )
