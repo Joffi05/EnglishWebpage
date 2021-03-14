e0bb03573bb313f5a9a95e960b5ad0acc8e27635
Joffi05 <79798896+Joffi05@users.noreply.github.com>
2021-03-14 14:13:27 +0100

    Delete News.js

--- server/News.js
+++
@@ -1,9 +0,0 @@
-const apiKey="0a317e68b6f04a00b363f018d435c100";
-let Topic="Bitcoins";
-let url='https://newsapi.org/v2/everything?q='+Topic+'&apiKey='+apiKey;
-
-fetch(url).then(res=>{
-    return res.json()
-}).then((data)=>{
-    console.log(data);
-})
