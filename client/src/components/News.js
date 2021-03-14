3f7339ec8b1bb50d7ca5003fb44f077c435a62ed
GenossenCoder <xperte9988@gmail.com>
2021-03-14 14:08:59 +0100

    News Funktion funkst jetzt richtig und ist gestyled

--- client/src/components/News.js
+++ client/src/components/News.js
@@ -3,7 +3,7 @@ import styles from '.././styles/News.module.css'
 import Typewriter from 'typewriter-effect'
 
 const News = () => {
-    const[data, setData] = React.useState('')
+    const[data, setData] = React.useState(null)
     
     const apiKey="de7a6efadb284f598012913141f52b42";
     let Topic="climate change";
@@ -18,25 +18,37 @@ const News = () => {
             setData(data)
         })
     }, [])
-    
+    let NewsJson=[]
     if(data){
-        let NewsJson = data.articles.slice(0,3)
-        console.log(NewsJson)
+        NewsJson = data.articles.slice(0,3)
 
     }
 
     return (
-        <form>
+        <div className={styles.NewsForm}>
             <h1 className={styles.Header}>News</h1>
-            <div className={styles.News1}>
+            <div className={styles.News}>
+                <h2 className={styles.NewsHeader}>{data ? NewsJson[0].title : null}</h2>
+                <span>{data ? NewsJson[0].author : null}</span>
+                <br/>
+                <br/>
+                <a  className={styles.Button} href={data ? NewsJson[0].url : null}>Link</a>
             </div>
-            <div className={styles.News2}>
-                
+             <div className={styles.News}>
+                <h2  className={styles.NewsHeader}>{data ? NewsJson[1].title : null}</h2>
+                <span>{data ? NewsJson[1].author : null}</span>
+                <br/>
+                <br/>
+                <a  className={styles.Button} href={data ? NewsJson[1].url : null}>Link</a>
             </div>
-            <div className={styles.News3}>
-                
-            </div>
-        </form>
+            <div className={styles.News}>
+                <h2  className={styles.NewsHeader}>{data ? NewsJson[2].title : null}</h2>
+                <span>{data ? NewsJson[2].author : null}</span>
+                <br/>
+                <br/>
+                <a className={styles.Button} href={data ? NewsJson[2].url : null}>Link</a>
+            </div> 
+        </div>
     )
 }
 
